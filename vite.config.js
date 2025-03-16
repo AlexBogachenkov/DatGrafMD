f4fe5b9920ff02882ae8d961c9cc95774e3fc4fd
Alexander <aleksbogac@gmail.com>
2025-03-16 02:04:13 +0200

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+export default {
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                page1a: '1a.html',
+                page3d: '3d.html'
+            }
+        }
+    }
+};
