901361cbf934b378f525028fdaf0785b1772a2d7
Alexander <aleksbogac@gmail.com>
2025-03-23 19:21:04 +0200

    3d labojumi

--- 3dScript.js
+++ 3dScript.js
@@ -8,9 +8,10 @@ for (let i = 0; i < fieldSize; i++) {
     }
     array.push(innerArray);
 }
+const table = document.getElementById("matrix");
 
 // linijas zīmēšana
-function drawLine(x0, y0, x1, y1) {
+function drawLine(x0, y0, x1, y1, cnt) {
     let dx = Math.abs(x1 - x0);
     let sx = x0 < x1 ? 1 : -1;
     let dy = -Math.abs(y1 - y0);
@@ -20,6 +21,7 @@ function drawLine(x0, y0, x1, y1) {
 
     while (true) {
         array[y0][x0] = true;
+        sides[cnt][1].push([x0, y0]);
         if (x0 === x1 && y0 === y1) {
             break;
         }
@@ -36,15 +38,18 @@ function drawLine(x0, y0, x1, y1) {
 }
 
 const points = [
-    [25, 5], [45, 10],
-    [55, 30], [35, 55],
-    [15, 50], [5, 30]
+    [25, 35], [35, 5], [42, 25],
+    [56, 30], [35, 57],
+    [15, 50], [5, 25]
 ];
+
+const sides = [];
 
 for (let i = 0; i < points.length; i++) {
     const [x0, y0] = points[i];
     const [x1, y1] = points[(i + 1) % points.length];
-    drawLine(x0, y0, x1, y1);
+    sides.push([[x0, y0, x1, y1],[]]);
+    drawLine(x0, y0, x1, y1, i);
 }
 
 // matricas kolonnu numurēšana
@@ -67,9 +72,6 @@ for (let i = 0; i < fieldSize; i++) {
     rowNumTable.appendChild(rowNumRow);
 }
 
-const table = document.getElementById("matrix");
-drawMatrix();
-
 // daudzstūra augšējās un apakšējās līnijas noteikšana
 let polygonTopLine = points[0][1];
 let polygonBottomLine = points[0][1];
@@ -82,34 +84,82 @@ points.forEach(point => {
     }
 })
 
-// daudzstūra tukšo (neaizpildīto) nogriežņu noteikšana un aizpildīšana
-for (let y = polygonTopLine; y <= polygonBottomLine; y++) {
-    const intersectionsWithX = [];
-    let isPreviousPointFilled = false;
-    let hasSectionStarted = false;
-    let sectionStart = -1;
-    for (let x = 0; x < fieldSize; x++) {
-        if (!array[y][x] && isPreviousPointFilled) {
-            hasSectionStarted = true;
-            sectionStart = x-1;
-        }
-        if (array[y][x] && !isPreviousPointFilled && hasSectionStarted) {
-            intersectionsWithX.push(sectionStart, x);
-            hasSectionStarted = false;
-            sectionStart = -1;
-        }
-        if (array[y][x]) {
-            isPreviousPointFilled = true;
-        } else {
-            isPreviousPointFilled = false;
-        }
+// katras malas minimalas(zemakas, tapec isteniba maksimalas) un maksimalas y vertibas atrasana
+sides.forEach(side => {
+    if (side[0][1] > side[0][3]) {
+        side.push(side[0][1]);
+        side.push(side[0][3]);
+    } else {
+        side.push(side[0][3]);
+        side.push(side[0][1]);
     }
-    for (let i = 0; i < intersectionsWithX.length; i+=2) {
-        for (let x = intersectionsWithX[i]; x <= intersectionsWithX[i+1]; x++) {
+})
+
+function getSidesThatStartAtY(y) {
+    return sides.filter(side => side[2] === y);
+}
+
+function getSidesThatEndsAtY(y) {
+    return sides.filter(side => side[3] === y);
+}
+
+// daudzstūra tukšo (neaizpildīto) nogriežņu noteikšana un aizpildīšana
+
+let activeSides = getSidesThatStartAtY(polygonBottomLine);
+const intersectionsWithX = [];
+for (let y = polygonBottomLine - 1; y > polygonTopLine; y--) {
+    getSidesThatStartAtY(y).forEach(side => activeSides.push(side));
+    const sidesToEnd = getSidesThatEndsAtY(y);
+    sidesToEnd.forEach(side => {
+        const index = activeSides.indexOf(side);
+        if (index > -1) {
+            activeSides.splice(index, 1);
+        }
+    })
+    intersectionsWithX.push([]);
+    for (let i = 0; i < activeSides.length; i++) {
+        intersectionsWithX[intersectionsWithX.length-1].push([]);
+        activeSides[i][1].filter(point => point[1] === y)
+            .sort((a, b) => {
+                if (a[0] < b[0]) {
+                    return -1;
+                }
+                if (a[0] > b[0]) {
+                    return 1;
+                }
+                return 0;
+            })
+            .forEach(point => {
+                intersectionsWithX[intersectionsWithX.length-1][i].push(point);
+            });
+    }
+}
+
+// punktu kārtošana
+intersectionsWithX.forEach(intersection => {
+    intersection.sort((a, b) => {
+        if (a[0][0] < b[0][0]) {
+            return -1;
+        }
+        if (a[0][0] > b[0][0]) {
+            return 1;
+        }
+        return 0;
+    });
+});
+
+// matricas aizpildīšana
+intersectionsWithX.forEach(intersection => {
+    for (let i = 0; i < intersection.length; i += 2) {
+        const x0 = intersection[i][0][0];
+        const l = intersection[i + 1].length;
+        const x1 = intersection[i + 1][l-1][0];
+        const y = intersection[i][0][1]
+        for (let x = x0; x < x1; x++) {
             array[y][x] = true;
         }
     }
-}
+})
 
 drawMatrix();
 
